e66b382690b656251c5fede797081635dd5bc784
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-08-20 14:36:53 +0700

    connect supabase 12332

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -42,6 +42,16 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Songs
+          </h1>  
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
