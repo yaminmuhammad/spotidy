c2f71bc26f3f7a84104a49041fe9570e683a5df8
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-04 14:26:42 +0700

    finish: finishing feature playbutton song

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
     songs: Song[];
@@ -18,6 +19,8 @@ const Library: React.FC<LibraryProps> = ({
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
+
+    const onPlay = useOnPlay(songs)
     
     const onClick = () => {
         if (!user) {
@@ -76,7 +79,7 @@ const Library: React.FC<LibraryProps> = ({
         ">
                 {songs.map((item) => (
                     <MediaItem
-                        onClick={() => { }}
+                        onClick={(id: string) => onPlay(id)}
                         key={item.id}
                         data={item} />
             ))}
